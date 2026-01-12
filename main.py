d7d6d19fff560ab1c9c6e469ab65df256b87cd3d
Nikhat Suleimanov <nikhat@example.com>
2026-01-12 09:50:04 +0100

    Fourth stage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnChestCancer import logger
 from cnnChestCancer.pipeline.first_stage_data_ingestion import DataIngestionTrainingPipeline
 from cnnChestCancer.pipeline.second_stage_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnChestCancer.pipeline.third_stage_model_trainer import ModelTrainingPipeline
+from cnnChestCancer.pipeline.fourth_stage_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -31,6 +32,17 @@ try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
+
+STAGE_NAME = "Evaluation stage"    
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
